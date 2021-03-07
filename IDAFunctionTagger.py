0bc6a585a661de66f942e05cecb01f7a1753e7c8
Alessandro Gario <alessandro.gario@gmail.com>
2021-03-07 19:49:59 +0100

    Updated for IDA 7.5

--- IDAFunctionTagger.py
+++ IDAFunctionTagger.py
@@ -2,54 +2,60 @@ import idc
 import json
 import webbrowser
 
-from idautils import *
-from idaapi import *
-from idaapi import PluginForm
+import ida_kernwin
+import idc
+import idautils
 
-from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-open_tag = "[TagList:"
-close_tag = "]"
-tag_separator = ","
+ACTION_NAME = "open-tag-manager"
+OPEN_TAG = "[TagList:"
+CLOSE_TAG = "]"
+TAG_SEPARATOR = ","
 
-class TagManager(object):
+
+class TagManager:
     def __init__(self):
         self.clear()
 
     def _addTagToFunction(self, function_name, tag_name):
-        global open_tag
-        global close_tag
-        global tag_separator
+        global OPEN_TAG
+        global CLOSE_TAG
+        global TAG_SEPARATOR
 
-        function_address = LocByName(function_name)
-        function_comment = GetFunctionCmt(function_address, 0)
+        function_address = idc.get_name_ea_simple(function_name)
+        function_comment = idc.get_func_cmt(function_address, False)
 
-        tag_list_start = function_comment.find(open_tag)
+        tag_list_start = function_comment.find(OPEN_TAG)
         if tag_list_start == -1:
-            SetFunctionCmt(function_address, function_comment + open_tag + tag_name + close_tag, 0)
+            set_func_cmt(function_address, function_comment +
+                         OPEN_TAG + tag_name + CLOSE_TAG, False)
             return
 
-        tag_list_end = function_comment.find(close_tag, tag_list_start)
+        tag_list_end = function_comment.find(CLOSE_TAG, tag_list_start)
         if tag_list_end == -1:
-            print("Malformed tag list found at address 0x%X" % function_address)
+            print("IDA Function Tagger: Malformed tag list found at address 0x%X" %
+                  function_address)
             return
 
-        tag_list = function_comment[tag_list_start : tag_list_end + 1]
+        tag_list = function_comment[tag_list_start: tag_list_end + 1]
         function_comment = function_comment.replace(tag_list, "")
 
-        tag_list = tag_list[len(open_tag) : len(tag_list) - len(close_tag)]
-        tag_list = tag_list.split(tag_separator)
+        tag_list = tag_list[len(OPEN_TAG): len(tag_list) - len(CLOSE_TAG)]
+        tag_list = tag_list.split(TAG_SEPARATOR)
 
         if tag_name not in tag_list:
             tag_list.append(tag_name)
+
         tag_list.sort()
 
-        function_comment = function_comment + open_tag
-        for tag in tag_list:
-            function_comment = function_comment + tag + tag_separator
-        function_comment = function_comment[ : -1] + close_tag
+        function_comment = function_comment + OPEN_TAG
 
-        SetFunctionCmt(function_address, function_comment, 0)
+        for tag in tag_list:
+            function_comment = function_comment + tag + TAG_SEPARATOR
+
+        function_comment = function_comment[: -1] + CLOSE_TAG
+        idc.set_func_cmt(function_address, function_comment, False)
 
     def scanDatabase(self, json_configuration):
         configuration = ""
@@ -57,81 +63,86 @@ class TagManager(object):
         try:
             configuration = json.loads(json_configuration)
         except:
-            print("Invalid configuration file")
+            print("IDA Function Tagger: Invalid configuration file")
             return
 
-        print("Loading configuration: %s" % configuration["name"])
-        print("Configuration comment: %s" % configuration["comment"])
+        print("IDA Function Tagger: Loading configuration: %s" %
+              configuration["name"])
+
+        print("IDA Function Tagger: Configuration comment: %s" %
+              configuration["comment"])
 
         for tag in configuration["tag_list"]:
-            print("Scanning for tag '%s'..." % tag["name"])
+            print("IDA Function Tagger: Scanning for tag '%s'..." %
+                  tag["name"])
+
             for imported_function in tag["import_list"]:
-                function_address = LocByName(str(imported_function))
+                function_address = idc.get_name_ea_simple(
+                    str(imported_function))
+
                 if function_address == BADADDR:
                     continue
 
-                cross_reference_list = CodeRefsTo(function_address, 0)
+                cross_reference_list = idautils.CodeRefsTo(function_address, 0)
                 for xref in cross_reference_list:
-                    function_name = GetFunctionName(xref)
+                    function_name = idc.get_func_name(xref)
                     self._addTagToFunction(function_name, str(tag["name"]))
 
     def removeAllTags(self):
-        global open_tag
-        global close_tag
+        global OPEN_TAG
+        global CLOSE_TAG
 
-        entry_point = BeginEA()
-        function_list = Functions(SegStart(entry_point), SegEnd(entry_point))
+        for function_address in idautils.Functions():
+            function_comment = idc.get_func_cmt(function_address, False)
 
-        for function_address in function_list:
-            function_comment = GetFunctionCmt(function_address, 0)
-
-            tag_list_start = function_comment.find(open_tag)
+            tag_list_start = function_comment.find(OPEN_TAG)
             if tag_list_start == -1:
                 continue
 
-            tag_list_end = function_comment.find(close_tag, tag_list_start)
+            tag_list_end = function_comment.find(CLOSE_TAG, tag_list_start)
             if tag_list_end == -1:
                 continue
 
-            SetFunctionCmt(function_address, function_comment.replace(function_comment[tag_list_start : tag_list_end + 1], ""), 0)
+            set_func_cmt(function_address, function_comment.replace(
+                function_comment[tag_list_start: tag_list_end + 1], ""), 0)
 
     def clear(self):
         self._tag_list = {}
         self._function_list = {}
 
     def update(self):
-        global open_tag
-        global close_tag
-        global tag_separator
+        global OPEN_TAG
+        global CLOSE_TAG
+        global TAG_SEPARATOR
 
         self.clear()
 
-        entry_point = BeginEA()
-        function_list = Functions(SegStart(entry_point), SegEnd(entry_point))
+        for function_address in idautils.Functions():
+            function_comment = idc.get_func_cmt(function_address, False)
 
-        for function_address in function_list:
-            function_comment = GetFunctionCmt(function_address, 0)
-
-            tag_list_start = function_comment.find(open_tag)
+            tag_list_start = function_comment.find(OPEN_TAG)
             if tag_list_start == -1:
                 continue
 
-            tag_list_end = function_comment.find(close_tag, tag_list_start)
+            tag_list_end = function_comment.find(CLOSE_TAG, tag_list_start)
             if tag_list_end == -1:
                 continue
 
-            tag_list = function_comment[tag_list_start + len(open_tag) : tag_list_end]
+            tag_list = function_comment[tag_list_start +
+                                        len(OPEN_TAG): tag_list_end]
             if len(tag_list) == 0:
                 continue
 
-            self._function_list[GetFunctionName(function_address)] = tag_list.split(tag_separator)
+            current_function_name = idc.get_func_name(function_address)
+            self._function_list[current_function_name] = tag_list.split(
+                TAG_SEPARATOR)
 
-            tag_list = tag_list.split(tag_separator)
+            tag_list = tag_list.split(TAG_SEPARATOR)
             for tag_name in tag_list:
                 if tag_name not in self._tag_list:
                     self._tag_list[tag_name] = []
 
-                self._tag_list[tag_name].append(GetFunctionName(function_address))
+                self._tag_list[tag_name].append(current_function_name)
 
     def tagList(self):
         return self._tag_list
@@ -139,15 +150,19 @@ class TagManager(object):
     def functionList(self):
         return self._function_list
 
-class TagViewer_t(PluginForm):
+
+class TagManagerInterface(ida_kernwin.PluginForm):
     def Update(self):
-        self._tag_list_model.clear();
-        self._function_list_model.clear();
+        self._tag_list_model.clear()
+        self._function_list_model.clear()
 
-        self._tag_list_model.setHorizontalHeaderLabels(["Tag", "Function", "Address"])
-        self._function_list_model.setHorizontalHeaderLabels(["Function", "Address", "Tags"])
+        self._tag_list_model.setHorizontalHeaderLabels(
+            ["Tag", "Function", "Address"])
 
-        for tag_name in self._tag_manager.tagList().iterkeys():
+        self._function_list_model.setHorizontalHeaderLabels(
+            ["Function", "Address", "Tags"])
+
+        for tag_name in self._tag_manager.tagList():
             tag = self._tag_manager.tagList()[tag_name]
 
             tag_item = QtGui.QStandardItem(tag_name)
@@ -156,16 +171,18 @@ class TagViewer_t(PluginForm):
             for function_name in tag:
                 function_name_item = QtGui.QStandardItem(function_name)
 
-                address = LocByName(function_name)
+                address = get_name_ea_simple(function_name)
                 address_item = QtGui.QStandardItem("0x%X" % address)
 
-                tag_item.appendRow([QtGui.QStandardItem(), function_name_item, address_item])
+                tag_item.appendRow(
+                    [QtGui.QStandardItem(), function_name_item, address_item])
 
-        for function_name in self._tag_manager.functionList().iterkeys():
+        for function_name in self._tag_manager.functionList():
             tag_list = self._tag_manager.functionList()[function_name]
 
             function_name_item = QtGui.QStandardItem(function_name)
-            address_item = QtGui.QStandardItem("0x%X" % LocByName(function_name))
+            address_item = QtGui.QStandardItem(
+                "0x%X" % get_name_ea_simple(function_name))
 
             tag_list_string = ""
             for tag in tag_list:
@@ -173,7 +190,8 @@ class TagViewer_t(PluginForm):
 
             tag_list_item = QtGui.QStandardItem(tag_list_string)
 
-            self._function_list_model.appendRow([function_name_item, address_item, tag_list_item])
+            self._function_list_model.appendRow(
+                [function_name_item, address_item, tag_list_item])
 
         self._function_list_view.expandAll()
         self._tag_list_view.expandAll()
@@ -192,7 +210,7 @@ class TagViewer_t(PluginForm):
                 pass
 
         elif model_index.column() == 1:
-            function_address = LocByName(str(model_index.data()))
+            function_address = idc.get_name_ea_simple(str(model_index.data()))
 
         else:
             return
@@ -209,7 +227,7 @@ class TagViewer_t(PluginForm):
                 pass
 
         elif model_index.column() == 0:
-            function_address = LocByName(str(model_index.data()))
+            function_address = idc.get_name_ea_simple(str(model_index.data()))
 
         else:
             return
@@ -221,7 +239,9 @@ class TagViewer_t(PluginForm):
         self.Update()
 
     def _onScanDatabaseClick(self):
-        file_path = QtWidgets.QFileDialog.getOpenFileName(self._parent_widget, 'Open configuration file', os.curdir, "*.json")
+        file_path = QtWidgets.QFileDialog.getOpenFileName(
+            self._parent_widget, 'Open configuration file', os.curdir, "*.json")
+
         if len(file_path[0]) == 0:
             return
 
@@ -239,13 +259,14 @@ class TagViewer_t(PluginForm):
         self.Update()
 
     def _onHomepageClick(self):
-        webbrowser.open("https://alessandrogar.io", new = 2, autoraise = True)
+        webbrowser.open("https://alessandrogar.io", new=2, autoraise=True)
 
     def _onClearFilterClick(self):
         self._filter_box.clear()
 
     def _onFilterTextChanged(self, text):
-        filter = QtCore.QRegExp(text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.Wildcard)
+        filter = QtCore.QRegExp(
+            text, QtCore.Qt.CaseInsensitive, QtCore.QRegExp.Wildcard)
 
         self._function_list_model_filter.setFilterRegExp(filter)
 
@@ -260,10 +281,15 @@ class TagViewer_t(PluginForm):
 
         self._function_list_model = QtGui.QStandardItemModel()
         self._function_list_model_filter = QtCore.QSortFilterProxyModel()
-        self._function_list_model_filter.setSourceModel(self._function_list_model)
+        self._function_list_model_filter.setSourceModel(
+            self._function_list_model)
         self._function_list_model_filter.setFilterKeyColumn(2)
 
         layout = QtWidgets.QVBoxLayout()
+
+        self._parent_widget = self.FormToPyQtWidget(parent_form)
+        self._parent_widget.setLayout(layout)
+
         filter_layout = QtWidgets.QHBoxLayout()
 
         text_label = QtWidgets.QLabel()
@@ -281,26 +307,34 @@ class TagViewer_t(PluginForm):
 
         layout.addLayout(filter_layout)
 
-        self._parent_widget = self.FormToPyQtWidget(parent_form)
         splitter = QtWidgets.QSplitter()
         layout.addWidget(splitter)
 
         self._tag_list_view = QtWidgets.QTreeView()
         self._tag_list_view.setAlternatingRowColors(True)
-        self._tag_list_view.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self._tag_list_view.setSelectionBehavior(
+            QtWidgets.QAbstractItemView.SelectRows)
+
         self._tag_list_view.setModel(self._tag_list_model)
         self._tag_list_view.setUniformRowHeights(True)
         self._tag_list_view.doubleClicked.connect(self._onTagClick)
-        self._tag_list_view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self._tag_list_view.setEditTriggers(
+            QtWidgets.QAbstractItemView.NoEditTriggers)
+
         splitter.addWidget(self._tag_list_view)
 
         self._function_list_view = QtWidgets.QTreeView()
         self._function_list_view.setAlternatingRowColors(True)
-        self._function_list_view.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self._function_list_view.setSelectionBehavior(
+            QtWidgets.QAbstractItemView.SelectRows)
+
         self._function_list_view.setModel(self._function_list_model_filter)
         self._function_list_view.setUniformRowHeights(True)
         self._function_list_view.doubleClicked.connect(self._onFunctionClick)
-        self._function_list_view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
+        self._function_list_view.setEditTriggers(
+            QtWidgets.QAbstractItemView.NoEditTriggers)
+
         splitter.addWidget(self._function_list_view)
 
         controls_layout = QtWidgets.QHBoxLayout()
@@ -327,30 +361,36 @@ class TagViewer_t(PluginForm):
         button.clicked.connect(self._onRemoveAllTagsClick)
         controls_layout.addWidget(button)
 
-        layout.addLayout(controls_layout)        
+        layout.addLayout(controls_layout)
 
         self.Update()
-
-        self._parent_widget.setLayout(layout)
 
     def OnClose(self, parent_form):
         return
 
     def Show(self):
-        return PluginForm.Show(self, "Function Tags", options = PluginForm.FORM_TAB)
+        return ida_kernwin.PluginForm.Show(self, "Tag Manager")
 
-def unloadScript():
-    global TagViewer
-    TagViewer.Close(0)
 
-    if not uninstallMenus():
-        print("Failed to uninstall the menus")
+class open_tag_manager_action_t(ida_kernwin.action_handler_t):
+    def __init__(self):
+        self._tag_manager_interface = TagManagerInterface()
 
-    del TagViewer
+    def activate(self, ctx):
+        self._tag_manager_interface.Show()
 
-def openTagViewer():
-    global TagViewer
-    TagViewer.Show()
+    def update(self, ctx):
+        return ida_kernwin.AST_ENABLE_FOR_WIDGET
 
-TagViewer = TagViewer_t()
-TagViewer.Show()
+
+ida_kernwin.register_action(
+    ida_kernwin.action_desc_t(
+        ACTION_NAME,
+        "Open the tag manager",
+        open_tag_manager_action_t(),
+        "Ctrl+Y"))
+
+if not ida_kernwin.attach_action_to_menu("View", ACTION_NAME, ida_kernwin.SETMENU_APP):
+    print("IDA Function Tagger: Failed to update the menus")
+
+print("IDA Function Tagger has been loaded; to open it, either use the 'View' menu or press CTRL+Y")
